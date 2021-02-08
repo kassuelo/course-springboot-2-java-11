0df19a7cbccf5900807a069f55e14251e6b93003
Kassuelo Moreira Okaszeski <kassuelo.mo@hotmail.com>
2021-02-07 22:51:10 -0300

    test solved error

--- src/main/java/com/educandoweb/course/CourseApplication.java
+++ src/main/java/com/educandoweb/course/CourseApplication.java
@@ -2,9 +2,15 @@ package com.educandoweb.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class CourseApplication {
+	
+    @RequestMapping("/")
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
